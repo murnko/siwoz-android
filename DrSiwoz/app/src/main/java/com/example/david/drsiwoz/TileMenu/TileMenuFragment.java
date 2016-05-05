7d8055fb420af560b51161ad4f20ad29adee2512
murnko <dawid.kamrowski@gmail.com>
2016-05-05 21:11:59 +0200

    simple result handler, testing textview

--- DrSiwoz/app/src/main/java/com/example/david/drsiwoz/TileMenu/TileMenuFragment.java
+++ DrSiwoz/app/src/main/java/com/example/david/drsiwoz/TileMenu/TileMenuFragment.java
@@ -9,15 +9,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.david.drsiwoz.R;
 
 
 public class TileMenuFragment extends Fragment implements TileMenuView{
+
+    public static final int RESULT_CANCELED = 0;
+    public static final int RESULT_OK = -1;
+    public static final int GET_PATIENT_CODE = 0;
+
+
     private Button patientButton;
     private Button drugsButton;
     private Button specialistReportsButton;
     private Button qrCodeScanButton;
+    private TextView texttest;
 
     private TileMenuPresenter presenter;
 
@@ -52,6 +60,7 @@ public class TileMenuFragment extends Fragment implements TileMenuView{
         drugsButton = (Button) rootView.findViewById(R.id.drugsButton);
         specialistReportsButton = (Button) rootView.findViewById(R.id.specialistReportsButton);
         qrCodeScanButton = (Button) rootView.findViewById(R.id.scanButton);
+        texttest = (TextView) rootView.findViewById(R.id.testQR);
 
         patientButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,7 +83,7 @@ public class TileMenuFragment extends Fragment implements TileMenuView{
                     Intent intent = new Intent("com.google.zxing.client.android.SCAN");
                     intent.putExtra("SCAN_MODE", "QR_CODE_MODE");
 
-                    startActivityForResult(intent, 0);
+                    startActivityForResult(intent,GET_PATIENT_CODE );
                 } catch (Exception e){
                     Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
                     Intent marketIntent = new Intent(Intent.ACTION_VIEW,marketUri);
@@ -100,5 +109,21 @@ public class TileMenuFragment extends Fragment implements TileMenuView{
 
         return rootView;
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == GET_PATIENT_CODE) {
 
-}
+            if (resultCode == RESULT_OK) { //Result_OK
+                String contents = data.getStringExtra("SCAN_RESULT");
+                mCallback.onMenuItemSelected(1);
+
+            }
+            if (resultCode == RESULT_CANCELED) { //Result_Canceled
+                //handle cancel
+            }
+        }
+    }
+
+    }
+
