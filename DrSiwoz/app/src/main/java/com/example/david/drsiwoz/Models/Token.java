0569aa14db45f1d50101e16dd8fd5f4889f5995a
Jacek Jankowski <jacek.j.jankowski@gmail.com>
2016-05-24 18:38:06 +0200

    Change token to small letter in API response.

--- DrSiwoz/app/src/main/java/com/example/david/drsiwoz/Models/Token.java
+++ DrSiwoz/app/src/main/java/com/example/david/drsiwoz/Models/Token.java
@@ -6,7 +6,7 @@ import com.google.gson.annotations.SerializedName;
  * Created by jacek on 18.04.16.
  */
 public class Token {
-    @SerializedName("Token")
+    @SerializedName("token")
     String mToken;
 
     public Token(String token) {
