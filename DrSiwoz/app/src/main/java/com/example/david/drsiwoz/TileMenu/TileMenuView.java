fa52976c19312d5ad8c7bbfa672c3f4fb945622d
Jacek Jankowski <jacek.j.jankowski@gmail.com>
2016-05-28 09:16:51 +0200

    Remove unused code.

--- DrSiwoz/app/src/main/java/com/example/david/drsiwoz/TileMenu/TileMenuView.java
+++ DrSiwoz/app/src/main/java/com/example/david/drsiwoz/TileMenu/TileMenuView.java
@@ -1,13 +1,7 @@
 package com.example.david.drsiwoz.TileMenu;
 
-import com.example.david.drsiwoz.Models.Patient;
-import com.example.david.drsiwoz.REST.RequestResult;
-
 /**
  * Created by david on 2016-03-19.
  */
 public interface TileMenuView {
-//    void updatePatient(Patient patient);
-//    void addPatientsResult(RequestResult result);
-//    void deletePatientsResult(RequestResult result);
 }
