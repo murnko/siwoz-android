77f65f20675e81d678c1f211ef1c00fbb4094d17
Jacek Jankowski <jacek.j.jankowski@gmail.com>
2016-05-28 09:15:18 +0200

    Use API from Gcloud server.

--- DrSiwoz/app/src/main/java/com/example/david/drsiwoz/AppConfig.java
+++ DrSiwoz/app/src/main/java/com/example/david/drsiwoz/AppConfig.java
@@ -4,5 +4,5 @@ package com.example.david.drsiwoz;
  * Created by david on 2016-03-19.
  */
 public class AppConfig {
-    public static String BaseUrl = "http://10.0.2.2:8000";
+    public static String BaseUrl = "http://104.155.73.108:8000";
 }
