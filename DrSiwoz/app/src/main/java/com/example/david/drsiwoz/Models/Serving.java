ccc5aec37158be140df6f037f2a3cf204d6a4ba1
murnko <dawid.kamrowski@gmail.com>
2016-06-07 18:25:29 +0200

    serving added

---
+++ DrSiwoz/app/src/main/java/com/example/david/drsiwoz/Models/Serving.java
@@ -0,0 +1,70 @@
+package com.example.david.drsiwoz.Models;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by david on 2016-06-07.
+ */
+public class Serving {
+        @SerializedName("id")
+        String id;
+
+        @SerializedName("name")
+        String name;
+
+        @SerializedName("serving")
+        float dosage;
+
+        @SerializedName("unit")
+        String unit;
+
+        @SerializedName("applied")
+        Boolean applied;
+
+        @SerializedName("canceled")
+        Boolean canceled;
+
+        @SerializedName("accepted")
+        Boolean accepted;
+
+        @SerializedName("suspended")
+        Boolean suspended;
+
+
+
+        public Serving( String id, String name, float serving, String unit, Boolean applied, Boolean canceled, Boolean accepted, Boolean suspended){
+            this.id = id;
+            this.name = name;
+            this.dosage = serving;
+            this.unit = unit;
+            this.applied = applied;
+            this.canceled = canceled;
+            this.accepted = accepted;
+            this.suspended = suspended;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public float getDosage() {
+            return dosage;
+        }
+
+        public String getUnit() {
+            return unit;
+        }
+
+        public Boolean isApplied() {return  applied;}
+
+        public Boolean isCanceled() {return canceled;}
+
+        public Boolean isAccepted() {return accepted;}
+
+        public Boolean isSuspended() {return suspended;}
+    }
+
