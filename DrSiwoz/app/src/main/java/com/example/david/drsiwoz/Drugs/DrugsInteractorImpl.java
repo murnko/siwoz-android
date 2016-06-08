9a9c6cec87f22691ebdafc206774871103fe342e
murnko <dawid.kamrowski@gmail.com>
2016-06-08 14:31:50 +0200

    fixed callback

--- DrSiwoz/app/src/main/java/com/example/david/drsiwoz/Drugs/DrugsInteractorImpl.java
+++ DrSiwoz/app/src/main/java/com/example/david/drsiwoz/Drugs/DrugsInteractorImpl.java
@@ -22,7 +22,7 @@ import retrofit2.Response;
 public class DrugsInteractorImpl implements DrugsInteractor {
 
     @Override
-    public void getServings(final OnGetDrugsListener listener,String patientId, String authToken) {
+    public void getServings(final OnGetDrugsListener listener,String authToken,String patientId) {
 
         Log.d("get Drugs", "started");
         Call<List<Serving>> call = ApiAuthProvider.getApi(authToken).getServings(patientId);
@@ -49,48 +49,52 @@ public class DrugsInteractorImpl implements DrugsInteractor {
     }
 
     @Override
-    public void applyDrug(OnGetDrugsListener listener, String authToken,String patientId, String appliedDrugId) {
+    public void applyDrug(final OnGetDrugsListener listener, String authToken,String patientId, String appliedDrugId) {
 
 
-        Call<ResponseBody> call = ApiAuthProvider.getApi(authToken).applyDrug(patientId, appliedDrugId);
-        call.enqueue(new Callback<ResponseBody>() {
+        Call<List<Serving>> call = ApiAuthProvider.getApi(authToken).applyDrug(patientId, appliedDrugId);
+        call.enqueue(new Callback<List<Serving>>() {
             @Override
-            public void onResponse(Call call, Response response) {
+            public void onResponse(Call<List<Serving>> call, Response<List<Serving>> response) {
                 int statusCode = response.code();
-                if (statusCode == 200) {
-                    Log.d("code", "OK200");
+                Log.d("status code", String.valueOf(statusCode));
+                if (statusCode != 200) {
+                    listener.onError();
                 } else {
-                    Log.d("code", "not 200");
+                    List<Serving> servings = response.body();
+                    listener.onSuccess(servings);
                 }
             }
 
             @Override
             public void onFailure(Call call, Throwable t) {
                 Log.d("bb", t.toString());
-                Log.d("bb", "onFailureDrugAplly");
+                Log.d("bb", "onFailureDrugUpdate");
             }
         });
     }
 
 
     @Override
-    public void updateServings(OnGetDrugsListener listener, String authToken,String patientId, UpServings upServingsList) {
-        Call<ResponseBody> call = ApiAuthProvider.getApi(authToken).updateServing(patientId, upServingsList);
-        call.enqueue(new Callback<ResponseBody>() {
+    public void updateServings(final OnGetDrugsListener listener, String authToken,String patientId, UpServings upServingsList) {
+        Call<List<Serving>> call = ApiAuthProvider.getApi(authToken).updateServing(patientId, upServingsList);
+        call.enqueue(new Callback<List<Serving>>() {
             @Override
-            public void onResponse(Call call, Response response) {
+            public void onResponse(Call<List<Serving>> call, Response<List<Serving>> response) {
                 int statusCode = response.code();
-                if (statusCode == 200) {
-                    Log.d("code", "OK200");
+                Log.d("status code", String.valueOf(statusCode));
+                if (statusCode != 200) {
+                    listener.onError();
                 } else {
-                    Log.d("code", "not 200");
+                    List<Serving> servings = response.body();
+                    listener.onSuccess(servings);
                 }
             }
 
             @Override
             public void onFailure(Call call, Throwable t) {
                 Log.d("bb", t.toString());
-                Log.d("bb", "onFailureDrugAplly");
+                Log.d("bb", "onFailureDrugUpdate");
             }
         });
     }
